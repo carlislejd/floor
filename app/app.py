11e9e4cb99a91f578446007210c8f94dbc488580
joshcarlisle <carlislejd@proton.me>
2022-12-13 16:21:39 -0500

    tried init structure

--- app/app.py
+++ app/app.py
@@ -2,16 +2,18 @@ import pandas as pd
 from flask import Flask, render_template
 
 
-app = Flask(__name__, static_url_path='/static')
 
 
-@app.route('/', methods=["GET", "POST"]) 
-def welcome():
-    """ 
-    Our about us page.
-    """
-    spread = pd.read_csv('spread.csv')
-    return render_template('base.html', tables=[spread.to_html(classes='data', header="true", index=False)])
+def create_app():
+    app = Flask(__name__, static_url_path='/static')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+    @app.route('/', methods=["GET", "POST"]) 
+    def welcome():
+        """ 
+        Our about us page.
+        """
+        spread = pd.read_csv('spread.csv')
+        return render_template('base.html', tables=[spread.to_html(classes='data', header="true", index=False)])
+
+    return app
