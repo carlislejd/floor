36f249d0e335cf6d0f3124af28c95853c901cdc2
joshcarlisle <carlislejd@proton.me>
2022-12-13 10:37:51 -0500

    added index file

--- app/app.py
+++ app/app.py
@@ -1,21 +1,18 @@
-import time
 import pandas as pd
-from flask import Flask, render_template, Response
+from flask import Flask, render_template
 
 
 
-
-app = Flask(__name__, static_url_path='/static')
-
-
-@app.route('/', methods=["GET", "POST"]) 
-def welcome():
-    """ 
-    Our about us page.
-    """
-    spread = pd.read_csv('spread.csv')
-    return render_template('base.html', tables=[spread.to_html(classes='data', header="true", index=False)])
+def create_app():
+  app = Flask(__name__, static_url_path='/static')
 
 
-if __name__ == '__main__':
-  app.run()
+  @app.route('/', methods=["GET", "POST"]) 
+  def welcome():
+      """ 
+      Our about us page.
+      """
+      spread = pd.read_csv('spread.csv')
+      return render_template('base.html', tables=[spread.to_html(classes='data', header="true", index=False)])
+
+  return app
