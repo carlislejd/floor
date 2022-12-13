0f0db1d10d9d36e59da93a4f2c88b3570f71a723
joshcarlisle <carlislejd@proton.me>
2022-12-13 09:51:33 -0500

    lets try hosting

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
-import re
+import time
 import pandas as pd
-from flask import Flask, render_template, request, make_response, send_file, send_from_directory
+from flask import Flask, render_template, Response
 
 
 
 
 app = Flask(__name__, static_url_path='/static')
+
 
 @app.route('/', methods=["GET", "POST"]) 
 def welcome():
@@ -17,4 +18,4 @@ def welcome():
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run()
