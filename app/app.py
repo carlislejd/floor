2dcb0d1488feaef302383deb2991019ec8b4e486
joshcarlisle <carlislejd@proton.me>
2022-12-13 11:01:55 -0500

    ughhh

--- app/app.py
+++ app/app.py
@@ -2,17 +2,14 @@ import pandas as pd
 from flask import Flask, render_template
 
 
-
-def create_app():
-  app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static')
 
 
-  @app.route('/', methods=["GET", "POST"]) 
-  def welcome():
-      """ 
-      Our about us page.
-      """
-      spread = pd.read_csv('spread.csv')
-      return render_template('base.html', tables=[spread.to_html(classes='data', header="true", index=False)])
+@app.route('/', methods=["GET", "POST"]) 
+def welcome():
+    """ 
+    Our about us page.
+    """
+    spread = pd.read_csv('spread.csv')
+    return render_template('base.html', tables=[spread.to_html(classes='data', header="true", index=False)])
 
-  return app
