c541a35a0a553879c5568c1d7a4ebb9c554821ff
joshcarlisle <carlislejd@proton.me>
2023-01-03 15:40:28 -0500

    that was dumb

--- project/data.py
+++ project/data.py
@@ -32,7 +32,6 @@ for c in contract_db:
         project = {}
         project['Collection'] = collection['name']
         project['7day Volume'] = round(collection['volume']['7day'], 2)
-        project['Lowest Floor Price'] = f"{str(collection['floorAsk']['sourceDomain'])} : {round(collection['floorAsk']['price']['amount']['decimal'], 3)} {collection['floorAsk']['price']['currency']['symbol']}"
         try:
             project['Lowest Floor Price'] = f"{str(collection['floorAsk']['sourceDomain'])} : {round(collection['floorAsk']['price']['amount']['decimal'], 3)} {collection['floorAsk']['price']['currency']['symbol']}"
             project['Highest Collection Offer'] = f"{str(collection['topBid']['sourceDomain'])} : {round(collection['topBid']['price']['amount']['decimal'], 3)} {collection['topBid']['price']['currency']['symbol']}"
